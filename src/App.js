4df2d780dbd9d7e1fbab177c45562232aff47d0b
n1ki4 <glazunov_n@ukr.net>
2020-10-25 18:20:18 +0200

    flexible lists

--- src/App.js
+++ src/App.js
@@ -5,26 +5,34 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'asdf', name: 'Max', age: 28 },
+      { id: 'fdsa', name: 'Manu', age: 29 },
+      { id: 'qwer', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} )
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
     // Метод splice() изменяет содержимое массива, удаляя существующие элементы и/или добавляя новые.
     // Синтаксис "array.splice(start, deleteCount[, item1[, item2[, ...]]])"
     persons.splice(personIndex, 1);
@@ -56,6 +64,8 @@ class App extends Component {
                   click={() =>this.deletePersonHandler(index)} 
                   name={person.name}
                   age={person.age}
+                  key={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}
                   />
         })}
       </div> 
